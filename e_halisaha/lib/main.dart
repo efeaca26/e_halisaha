73e51f509967f845eb54bee66b3fa7b8151a7434
efeaca26 <efeaca26@gmail.com>
2026-02-02 22:00:12 +0300

    Update main.dart

--- e_halisaha/lib/main.dart
+++ e_halisaha/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'ekranlar/giris/giris_ekrani.dart';
 
-// TEMA YÖNETİCİSİ (Global Değişken)
 ValueNotifier<ThemeMode> temaYoneticisi = ValueNotifier(ThemeMode.light);
 
 void main() {
@@ -22,7 +21,6 @@ class EHalisahaUygulamasi extends StatelessWidget {
           title: 'E-HalıSaha',
           debugShowCheckedModeBanner: false,
           
-          // --- TEMA AYARLARI ---
           themeMode: currentMode,
           theme: ThemeData(
             brightness: Brightness.light,
@@ -33,21 +31,18 @@ class EHalisahaUygulamasi extends StatelessWidget {
               foregroundColor: Colors.black,
               elevation: 0
             ),
-            // Diğer light tema ayarları...
           ),
           darkTheme: ThemeData(
             brightness: Brightness.dark,
             primaryColor: const Color(0xFF22C55E),
-            scaffoldBackgroundColor: const Color(0xFF111827), // Koyu gri
+            scaffoldBackgroundColor: const Color(0xFF111827),
             appBarTheme: const AppBarTheme(
               backgroundColor: Color(0xFF1F2937),
               foregroundColor: Colors.white,
               elevation: 0
             ),
-            // Diğer dark tema ayarları...
           ),
           
-          // --- DİL AYARLARI ---
           localizationsDelegates: const [
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
